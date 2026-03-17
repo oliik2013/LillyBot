a400fa0e826d6e42c3b3ed2f455225a3f72ad4dc
oliik <131269027+oliik2013@users.noreply.github.com>
2026-03-17 20:42:45 +0100

    Update redis.ts

--- src/utils/redis.ts
+++ src/utils/redis.ts
@@ -1,7 +1,14 @@
 import { Ratelimit } from "@upstash/ratelimit";
 import { Redis } from "@upstash/redis";
 
-export const redis = Redis.fromEnv();
+import 'dotenv/config'
+
+// export const redis = Redis.fromEnv();
+
+export const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL!,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN!,
+});
 
 export const ratelimit = new Ratelimit({
   redis: redis,
